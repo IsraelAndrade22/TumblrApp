949cb86cbe19293beddd6a363df8805a02e3e9a1
Israel Andrade <israel_andrade@Israels-MacBook-Pro.local>
2018-01-31 15:29:50 -0800

    add pods

---
+++ TumblrApp/PhotosViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PhotosViewController.swift
+//  TumblrApp
+//
+//  Created by Israel Andrade on 1/29/18.
+//  Copyright © 2018 Israel Andrade. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    var posts: [[String: Any]] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
